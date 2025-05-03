c9ef5dd026545cd5ddfa88983f8eb61c2b06b8a0
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-05-03 10:09:30 +0600

    complete four chapter js condition

---
+++ FourChapter/logical_not.js
@@ -0,0 +1,17 @@
+const buyProduct = 2000;
+let cashback = 0;
+
+cashback = buyProduct >= 3000? cashback + 500:cashback + 0; 
+console.log(cashback); 
+
+let age = 10;
+age = age > 15?console.log("Teenager"):console.log("Children");
+
+let isLogin = false;
+isLogin = isLogin === true?console.log("Welcome back!"):console.log("Please login to continue.");
+
+let tank = false;
+tank = tank === true?console.log("Tank is full!"):console.log("Tank is empty!");
+
+let isPassed = true;
+isPassed = isPassed === true?console.log("You passed the exam!"):console.log("Semester e serious study korbo");
