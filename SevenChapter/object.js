1a2c0ca7a8896c5ad2c52f3beb9e599b354c0658
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-05-04 11:59:25 +0600

    added object and object porperty

---
+++ SevenChapter/object.js
@@ -0,0 +1,52 @@
+const teacher = {
+    name: "Nadim Mostofa",
+    age: 21,
+    subject: "JavaScript",
+    isMarried: false,
+    address: {
+        city: "Dhaka",
+        country: "Bangladesh",
+    },
+}
+console.log(teacher)
+
+const tree = {
+    name: "antiybox",
+    age: 234 ,
+    hight : "5.5 fit",
+    color: "green",
+}
+console.log(tree)
+
+const father = {
+    name: "Fozlul Haque",
+    age: 40,
+    profession:"worker",
+    children: 4
+}
+console.log(father)
+
+const motorcycle = {
+    name: "Yamaha",
+    model: "FZ",
+    color: "black",
+    price: 200000,
+}
+console.log(motorcycle)
+
+const bride = {
+    name: "monyna",
+    color:"as",
+    weight: "250 gm",
+    type: nature
+}
+console.log(bride)
+
+const laptop = {
+    name: "Dell",
+    model: "Inspiron",
+    color: "black",
+    price: 50000,
+    processor: "Intel i7",
+}
+console.log(laptop)
---
+++ SevenChapter/object_porperty.js
@@ -0,0 +1,54 @@
+const player = {
+    name: "John",
+    age: 25,
+    team: "Team A",
+    position: "Forward",
+    goals: 10,
+}
+console.log(player.position)
+
+const laptop = {
+    name: "Dell",
+    model: "XPS 13",
+    color: "Silver",
+    price: 1200,
+    processor: "Intel i7",
+    ram: "16GB",
+    storage: "512GB SSD",
+}
+console.log(laptop.storage)
+
+const favPlace = {
+    name: "Paris",
+    country: "France",
+    attractions: ["Eiffel Tower", "Louvre Museum", "Notre-Dame Cathedral"],
+    bestTimeToVisit: "Spring",
+}
+console.log(favPlace.attractions[2])
+
+const phone = {
+    brand: "Apple",
+    model: "iPhone 14",
+    color: "Black",
+    storage: "256GB",
+    camera: "12MP",
+
+}
+console.log(phone.camera)
+
+const college = {
+    name: "ABC College",
+    location: "New York",
+    established: 1990,
+    courses: ["Computer Science", "Business", "Arts"],
+    students: 2000,
+}
+console.log(college.courses[1])
+
+const movie = {
+    name: "Inception",
+    director: "Nolan",
+    rating: 9,
+
+}
+console.log(movie.rating)
