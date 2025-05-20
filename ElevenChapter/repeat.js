1c1f189381320197a693c2a7c1409eeea93178b7
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-05-20 20:03:06 +0600

    complete js string methond added repeat and revers problem sovle

---
+++ ElevenChapter/repeat.js
@@ -0,0 +1,8 @@
+const text = "Hi";
+console.log("repeat 10 this string:", text.repeat(10))
+
+const text2 = "I am learn javaScript! javaScript is amazing!"
+console.log("Replace javaScript to js:", text2.replace("javaScript", "js"))
+
+const text3 = " I love apples and apples are testy";
+console.log(text3.replaceAll("apples", "mangoes"))
