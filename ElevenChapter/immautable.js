3e97f7092b30288ce6f28e769351f4f963afa2dc
Nadim Mostofa <nadimmostafa334@gmail.com>
2025-05-20 18:13:57 +0600

    added js string methon concept problem solve

---
+++ ElevenChapter/immautable.js
@@ -0,0 +1,16 @@
+const city = "Chittogram"
+console.log("index of g:",city.indexOf("g"))
+
+const division = "Sylhet"
+console.log("Finding y letter in variable", division.includes("y"))
+
+const name = "Rifat";
+console.log("last character in string:", name[name.length - 1])
+
+const language = "javaScript";
+console.log("index of string:", language.indexOf("rip"))
+
+const text = 'immutable';
+console.log("length of string:", text.length)
+text[4] = "F";
+console.log("check string is mutable or immutable:", text)
---
+++ ElevenChapter/lower&upper.js
@@ -0,0 +1,13 @@
+const email = "User@Example.Com"
+console.log("Convert string in lower case:", email.toLowerCase())
+
+const greeting = "hElLo WoRld";
+console.log("convert string to lower case:", greeting.toLowerCase())
+console.log("convert string to upper case:", greeting.toUpperCase())
+
+const language = "javaScript";
+if(language.toLowerCase().includes("script") === true){
+    console.log("script is true ")
+}else{
+    console.log("script is false")
+}
